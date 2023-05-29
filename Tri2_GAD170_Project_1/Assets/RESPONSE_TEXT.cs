fe9eb5ec9e804ed2367d11c54cdf474844f83c7a
SAE1\1027859 <1027859@student.sae.edu.au>
2023-05-29 18:36:06 +1000

    Unity Project

---
+++ Tri2_GAD170_Project_1/Assets/RESPONSE_TEXT.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RESPONSE_TEXT : MonoBehaviour
+{       
+    [TextAreaAttribute(15,20)]
+    public string TEXT;
+}
